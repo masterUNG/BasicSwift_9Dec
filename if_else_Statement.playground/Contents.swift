299d35ac2ab8c328f0f06f79a6e2b610d6faad3f
MasterUNG <phrombutr@gmail.com>
2017-12-10 10:27:53 +0700

    if else Statement

---
+++ if_else_Statement.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let ageInt = 15
+
+if (ageInt<18) {
+//    True
+    print("You are Young")
+} else {
+//    False
+    print("You are Old")
+}
+
+let nameString = "Doramon"
+let friendStrings = ["Doramon", "Nopita"]
+
+if (nameString == friendStrings[1]) {
+    print("My Friend is \(nameString)")
+}   else {
+    print("Not my Friend")
+}
+
+
+
+
+
+
+
