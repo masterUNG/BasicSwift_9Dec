8ac7b5de388410df6e345197f718c3d330d6f4ab
MasterUNG <phrombutr@gmail.com>
2017-12-10 11:47:10 +0700

    Optional OK

---
+++ Optional.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Explicit Varialble to String Data type
+var numberString: String = "six"
+
+//Change Data Type String ==> Integer
+var numberInt = Int(numberString)
+//var answerInt = numberInt! + 10
+
+//Solution for nil
+if let myNumberInt = numberInt {
+    
+    var answerInt = myNumberInt + 10
+    
+}   // if
+
+
+
+
