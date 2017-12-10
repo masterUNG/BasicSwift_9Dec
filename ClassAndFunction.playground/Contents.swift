41739ab90ff6b6321ad9a3d4006c0cc88f961c37
MasterUNG <phrombutr@gmail.com>
2017-12-10 13:40:04 +0700

    Inheriate Resource

---
+++ ClassAndFunction.playground/Contents.swift
@@ -0,0 +1,42 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Create Class
+class MyClass {
+    
+//    Explicit
+    var numberInt = 123
+    var nameString = "Doramon"
+    
+
+}   // MyClass
+
+//How to Call Resource of MyClass
+//Inherited Class
+var myClass = MyClass()
+
+var myNumber = myClass.numberInt
+let myName = myClass.nameString
+
+print("before Number ==> \(myClass.numberInt) ")
+
+//การกำหนดค่าให้ ตัวแปร ที่อยู่นอกคลาส
+myClass.numberInt = 1000
+
+print("After Number ==> \(myClass.numberInt) ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
